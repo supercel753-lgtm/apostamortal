82199e176587aa2edd3577ec3d5602c8c20db723
supercel753-lgtm <supercel753@gmail.com>
2026-08-30 13:34:45 -0300

    Update Game.java

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,1 +1,23 @@
+```java
+package game;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("          LAST CHANCE");
+        System.out.println("====================================");
+        System.out.println();
+
+        System.out.println("Uma noite.");
+        System.out.println("Uma sala.");
+        System.out.println("Duas pessoas.");
+        System.out.println("E um dispositivo que não deveria existir.");
+        System.out.println();
+
+        Game game = new Game();
+        game.start();
+    }
+}
+```
